8ac8e1bc9dcab6120fb696cdbfc4ca1f164aacae
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-08-01 15:55:54 +0900

    chore: 로컬 폰트 설정 (#20)

---
+++ src/app/fonts.ts
@@ -0,0 +1,19 @@
+import localFont from 'next/font/local'
+
+export const nanumBarunGothic = localFont({
+  src: [
+    {
+      path: './assets/fonts/nanumBarunGothic/NanumBarunGothicBold.ttf',
+      weight: '600',
+    },
+    {
+      path: './assets/fonts/nanumBarunGothic/NanumBarunGothic.ttf',
+      weight: '400',
+    },
+    {
+      path: './assets/fonts/nanumBarunGothic/NanumBarunGothicLight.ttf',
+      weight: '300',
+    },
+  ],
+  variable: '--font-nanumBarunGothic',
+})
