c7ead2579ca87cab8937aa5f37e193d9e821dd4a
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-08-06 15:03:37 +0900

    chore: tailwindcss theme 설정 (#20)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,22 @@
 import type { Config } from 'tailwindcss'
 
+import { borderRadius, boxShadow, color, fontSize, fontWeight, opacity } from '@/styles/theme'
+
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/stories/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/features/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      borderRadius,
+      boxShadow,
+      color,
+      fontSize,
+      fontWeight,
+      opacity,
       fontFamily: {
         nanumBarunGothic: ['--font--nanumBarunGothic'],
       },
