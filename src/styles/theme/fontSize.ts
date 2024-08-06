af1f95a9b6e202617042bb3c35545698a08771c3
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-08-06 15:02:16 +0900

    chore: fontSize 토큰 추가 (#20)

---
+++ src/styles/theme/fontSize.ts
@@ -0,0 +1,10 @@
+export const fontSize = {
+  xl: '25px',
+  lg: '20px',
+  md: '15px',
+  sm: '13px',
+  xs: '11px',
+  '2xs': '10px',
+  '3xs': '9px',
+  '4xs': '8px',
+}
--- src/styles/theme/index.ts
+++ src/styles/theme/index.ts
@@ -1,3 +1,4 @@
 export { borderRadius } from './borderRadius'
 export { boxShadow } from './boxShadow'
 export { color } from './color'
+export { fontSize } from './fontSize'
