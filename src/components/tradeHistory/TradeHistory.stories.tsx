70011a4bed8c61cc67e7b63a1176a0375597a500
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2025-02-15 08:07:55 +0000

    feat: tanstack-table 추가 (#40)

---
+++ src/components/tradeHistory/TradeHistory.stories.tsx
@@ -0,0 +1,71 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { TradeHistory } from './TradeHistory'
+
+const meta: Meta<typeof TradeHistory> = {
+  title: 'TradeHistory',
+  component: TradeHistory,
+  tags: ['autodocs'],
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+// mockData
+const tradeHistoryData = [
+  {
+    derivativeName: '가나다라마바사아자차카타',
+    optionType: 'PUT',
+    tradePrice: 400.06,
+    commission: 0.06,
+    settlementAmout: 400,
+    time: new Date('2024-08-07T01:00:00'),
+  },
+  {
+    derivativeName: '가나다라마바사아자차카타',
+    optionType: 'CALL',
+    tradePrice: 400.06,
+    commission: 0.06,
+    settlementAmout: 400,
+    time: new Date('2024-08-07T00:00:00'),
+  },
+  {
+    derivativeName: '가나다라마바사아자차카타',
+    optionType: 'CALL',
+    tradePrice: 400.06,
+    commission: 0.06,
+    settlementAmout: 400,
+    time: new Date('2024-08-07T00:00:00'),
+  },
+  {
+    derivativeName: '가나다라마바사아자차카타',
+    optionType: 'CALL',
+    tradePrice: 400.06,
+    commission: 0.06,
+    settlementAmout: 400,
+    time: new Date('2024-08-07T00:00:00'),
+  },
+  {
+    derivativeName: '가나다라마바사아자차카타',
+    optionType: 'PUT',
+    tradePrice: 400.06,
+    commission: 0.06,
+    settlementAmout: 400,
+    time: new Date('2024-08-07T00:00:00'),
+  },
+  {
+    derivativeName: '가나다라마바사아자차카타',
+    optionType: 'CALL',
+    tradePrice: 400.06,
+    commission: 0.06,
+    settlementAmout: 400,
+    time: new Date('2024-08-07T00:00:00'),
+  },
+]
+
+export const Default: Story = {
+  args: {
+    tradeHistoryData: tradeHistoryData,
+  },
+}
---
+++ src/components/tradeHistory/TradeHistory.tsx
@@ -0,0 +1,108 @@
+'use client'
+
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
+
+import type { TradeHistoryType } from '@/types'
+import { formatDate } from '@/utils'
+
+const columnHelper = createColumnHelper<TradeHistoryType>()
+const columns = [
+  columnHelper.accessor('derivativeName', {
+    header: '종목이름',
+    cell: (props) => <p>{props.getValue()}</p>,
+    size: 54,
+  }),
+  columnHelper.accessor('optionType', {
+    header: '종류',
+    cell: (props) => <p className="text-center">{props.getValue()}</p>,
+    size: 42,
+  }),
+  columnHelper.accessor('tradePrice', {
+    header: '거래금액',
+    cell: (props) => (
+      <p className="text-center">
+        {props.getValue()?.toLocaleString('ko-KR', { maximumFractionDigits: 2 })}
+      </p>
+    ),
+    size: 62,
+  }),
+  columnHelper.accessor('commission', {
+    header: '수수료',
+    cell: (props) => (
+      <p className="text-center">
+        {props.getValue()?.toLocaleString('ko-KR', { maximumFractionDigits: 2 })}
+      </p>
+    ),
+    size: 62,
+  }),
+  columnHelper.accessor('settlementAmout', {
+    header: '정산금',
+    cell: (props) => (
+      <p className="text-center">
+        {props.getValue()?.toLocaleString('ko-KR', { maximumFractionDigits: 2 })}
+      </p>
+    ),
+    size: 62,
+  }),
+  columnHelper.accessor('time', {
+    header: '주문시간',
+    cell: (props) => <p className="text-center">{formatDate(props.getValue())}</p>,
+    size: 47,
+  }),
+]
+
+interface TradeHistoryProps {
+  tradeHistoryData: TradeHistoryType[]
+}
+
+export const TradeHistory = ({ tradeHistoryData: data }: TradeHistoryProps) => {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
+  return (
+    <table className="w-full">
+      <thead className="border-b border-gray-1">
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th
+                key={header.id}
+                style={{
+                  width: `${header.getSize()}px`,
+                }}
+                className="body3_bold pb-[8px] text-black"
+              >
+                {flexRender(header.column.columnDef.header, header.getContext())}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id} className="h-[40px] border-b border-gray-1">
+            {row.getVisibleCells().map((cell) => (
+              <td
+                key={cell.id}
+                style={{
+                  width: `${cell.column.getSize()}px`,
+                }}
+                className="body5_reg text-black"
+              >
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
