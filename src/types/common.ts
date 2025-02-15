1b94cebebb4febe3736318602505ae8bc5c36b6a
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2025-02-15 07:59:11 +0000

    feat: tanstack-table 추가 (#40)

--- src/types/common.ts
+++ src/types/common.ts
@@ -7,3 +7,12 @@ export interface BottomSheetType {
   isShowing: boolean
   onClickScrim: VoidFunction
 }
+
+export interface TradeHistoryType {
+  derivativeName: string
+  optionType: 'CALL' | 'PUT' | string // 수정 필요
+  tradePrice: string
+  commission: string
+  settlementAmout: string
+  time: Date
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export * from './calculatePercentage'
+export * from './formatDate'
