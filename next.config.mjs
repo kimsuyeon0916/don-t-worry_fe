18047f0f384c36820b98e3820fde428c0f5e2cbc
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2025-02-19 01:15:31 +0000

    feat: next config 내, maximumFileSizeToCacheInBytes 설정
    - 빌드 시 폰트 용량으로 인한 미리 캐싱(precache) 오류 해결

--- next.config.mjs
+++ next.config.mjs
@@ -13,4 +13,7 @@ const prod = process.env.NODE_ENV === 'production'
 export default withPWA({
   dest: 'public',
   disable: prod ? false : true,
+  workbox: {
+    maximumFileSizeToCacheInBytes: 5000000, // 5MB로 증가 (기본값: 2MB)
+  },
 })(nextConfig)
