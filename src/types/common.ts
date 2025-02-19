b1cec03c72ac32c15784d77b4c7b2f1b9f67e91c
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2025-02-19 00:28:15 +0000

    feat: TradeHistoryType 타입 변경
    - tradePrice, commission, settlementAmount 프로퍼티 타입 string -> number 변경

--- src/types/common.ts
+++ src/types/common.ts
@@ -11,8 +11,8 @@ export interface BottomSheetType {
 export interface TradeHistoryType {
   derivativeName: string
   optionType: 'CALL' | 'PUT' | string // 수정 필요
-  tradePrice: string
-  commission: string
-  settlementAmout: string
+  tradePrice: number
+  commission: number
+  settlementAmount: number
   time: Date
 }
